47a9b4f06caaf5cb5cb304dcbb60d4ebaf8f0750
Sigurður Smárason <runningman69@gmail.com>
2018-10-28 22:09:21 +0100

    updating roxygen docs

--- R/runExample.R
+++ R/runExample.R
@@ -1,11 +1,11 @@
-#' @name runExample
+#' @name runDirinputExample
 #' 
 #' @title Runs a demo app with the code
 #' 
 #' See the code in the example_application folder
 #' 
 #' @export
-runExample <- function() {
+runDirinputExample <- function() {
   appDir <- system.file("example_application", package = "shinyDirectoryInput")
   if (appDir == "") {
     stop("Could not find example directory. Try re-installing `shinyDirectoryInput`.", call. = FALSE)
